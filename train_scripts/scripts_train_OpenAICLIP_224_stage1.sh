2ee928fbd11f44ee74e90da31c4b3fefd0e8b4a9
Tommy-Xin <xinhku168@gmail.com>
2026-04-15 13:17:53 +0000

    first upload

---
+++ train_scripts/scripts_train_OpenAICLIP_224_stage1.sh
@@ -0,0 +1,3 @@
+export CUDA_VISIBLE_DEVICES=4,5
+accelerate launch --config_file "train_configs/accelerate_config.yaml" \
+train_OpenAICLIP_stage1.py --config "train_configs/train_OpenAICLIP_224_stage1.yaml"
