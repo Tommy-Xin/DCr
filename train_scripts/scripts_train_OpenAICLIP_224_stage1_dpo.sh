7ac1be23bacd5228aabf6f8b4d4c92002352c7e8
Tommy-Xin <xinhku168@gmail.com>
2026-04-16 12:20:31 +0800

    revise

--- train_scripts/scripts_train_OpenAICLIP_224_stage1_dpo.sh
+++ train_scripts/scripts_train_OpenAICLIP_224_stage1_dpo.sh
@@ -1,2 +1,3 @@
+export CUDA_VISIBLE_DEVICES=0
 accelerate launch --config_file "train_configs/accelerate_config.yaml" \
 train_OpenAICLIP_stage1_dpo.py --config "train_configs/train_OpenAICLIP_224_stage1_dpo.yaml"
